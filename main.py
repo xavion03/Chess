5296e4b8e309e4dea22d40c7c2faa89056ab32e5
xavion03 <66435250+xavion03@users.noreply.github.com>
2024-02-04 18:03:08 +0000

    Implemented most of basic evaluation

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import chess
 
+#import piece tables from piece_tables file
+from piece_tables import *
+
+#create chess board object
 board = chess.Board()
 
-print(board)
-print(board.legal_moves)
+#function to evaluate board
+#returns evaluation score
+def evaluate_board():
+    #taken from https://andreasstckl.medium.com/writing-a-chess-program-in-one-day-30daff4610ec
+
+    #variables to hold number of pieces currently on board
+    wp = len(board.pieces(chess.PAWN, chess.WHITE)) 
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    #calculate material based on how many pieces are on board
+    #based off material values from simplified evaluation function (https://www.chessprogramming.org/Simplified_Evaluation_Function#Piece_Values)
+    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq= pawnsq + sum([-pawntable[chess.square_mirror(i)] for i in board.pieces(chess.PAWN, chess.BLACK)])
+
+num = chess.E2
+num
+
+num2 = pawntable[num]
+num2
 
 
-
-move = chess.Move(chess.E2, chess.E4)
-if move in board.legal_moves: 
-    board.push(move)
-    print(board)
-else:
-    print ("That move is not legal")
 
 
