fdad4bc87e3b9564048d0b7132b1c14e0209ddb7
xavion03 <66435250+xavion03@users.noreply.github.com>
2024-02-04 05:38:57 +0000

    The first test with python-chess

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+import chess
+
+board = chess.Board()
+
+print(board)
+print(board.legal_moves)
+
+
+
+move = chess.Move(chess.E2, chess.E4)
+if move in board.legal_moves: 
+    board.push(move)
+    print(board)
+else:
+    print ("That move is not legal")
+
+
